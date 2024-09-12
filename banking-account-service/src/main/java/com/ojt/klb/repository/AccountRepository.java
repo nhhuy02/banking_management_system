2f3022755731ada7ff6bfc742c9f28a46d26af4f
Nguyễn Quốc Duy <127750881+Duy220807@users.noreply.github.com>
2024-09-12 14:26:17 +0700

    update

--- banking-account-service/src/main/java/com/ojt/klb/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ojt.klb.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ojt.klb.model.Account;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-    boolean existsByAccountNumber(String accountNumber);
-}
