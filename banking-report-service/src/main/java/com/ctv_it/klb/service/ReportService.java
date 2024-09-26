5cdcb992031d5118f891d2f468f976d1818314b5
vansi1804 <vansi18042001@gmail.com>
2024-09-26 16:59:45 +0700

    add excel export

--- banking-report-service/src/main/java/com/ctv_it/klb/service/ReportService.java
+++ banking-report-service/src/main/java/com/ctv_it/klb/service/ReportService.java
@@ -2,6 +2,7 @@
 
 package com.ctv_it.klb.service;
 
+import com.ctv_it.klb.common.Default.File;
 import com.ctv_it.klb.config.exception.InvalidExceptionCustomize;
 import com.ctv_it.klb.dto.request.ReportRequestDTO;
 import com.ctv_it.klb.enumeration.ReportFormat;
@@ -9,6 +10,7 @@ import com.ctv_it.klb.factory.ReportFormatServiceFactory;
 import com.ctv_it.klb.factory.ReportTypeServiceFactory;
 import com.ctv_it.klb.service.format.ReportFormatService;
 import com.ctv_it.klb.service.type.ReportTypeService;
+import com.ctv_it.klb.util.FileUtil;
 import java.util.Collections;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,22 +23,19 @@ public class ReportService {
 
   private final ReportTypeServiceFactory reportTypeServiceFactory;
   private final ReportFormatServiceFactory reportFormatServiceFactory;
+  private final FileUtil fileUtil;
 
-  public Object report(com.ctv_it.klb.dto.request.ReportRequestDTO requestDTO) {
+  public Object report(ReportRequestDTO requestDTO) {
     // check support type
-    ReportTypeService<?> reportTypeService = reportTypeServiceFactory
-        .getReportTypeService(requestDTO.getReportType());
+    ReportTypeService<?> reportTypeService = reportTypeServiceFactory.getReportTypeService(
+        requestDTO.getReportType());
 
     if (reportTypeService == null) {
-      throw new InvalidExceptionCustomize(
-          Collections.singletonList(
-              com.ctv_it.klb.dto.response.ErrorDetailDTO.builder()
-                  .field("reportType")
-                  .rejectedValue(requestDTO.getReportType())
-                  .message("Report for type '" + requestDTO.getReportType() + "' is not supported")
-                  .build()
-          )
-      );
+      throw new InvalidExceptionCustomize(Collections.singletonList(
+          com.ctv_it.klb.dto.response.ErrorDetailDTO.builder().field("reportType")
+              .rejectedValue(requestDTO.getReportType())
+              .message("Report for type '" + requestDTO.getReportType() + "' is not supported")
+              .build()));
     }
 
     // Return Object if request format NONE
@@ -47,28 +46,30 @@ public class ReportService {
           requestDTO.getReportType(), requestDTO.getReportFormat());
 
       if (reportFormatService == null) {
-        throw new InvalidExceptionCustomize(
-            Collections.singletonList(
-                com.ctv_it.klb.dto.response.ErrorDetailDTO.builder()
-                    .field("reportFormat")
-                    .rejectedValue(requestDTO.getReportFormat())
-                    .message("Report format '" + requestDTO.getReportFormat() + "' for type '"
-                        + requestDTO.getReportType() + "' is not supported")
-                    .build()
-            )
-        );
+        throw new InvalidExceptionCustomize(Collections.singletonList(
+            com.ctv_it.klb.dto.response.ErrorDetailDTO.builder().field("reportFormat")
+                .rejectedValue(requestDTO.getReportFormat()).message(
+                    "Report format '" + requestDTO.getReportFormat() + "' for type '"
+                        + requestDTO.getReportType() + "' is not supported").build()));
       } else {
-        return exportReport(search(requestDTO, reportTypeService), reportFormatService);
+        String fileName = File.TARGET_PATH + "/" + generateFileName(requestDTO);
+        return exportReport(fileName, search(requestDTO, reportTypeService), reportFormatService);
       }
     }
   }
 
   @SuppressWarnings("unchecked")
-  private <T> Object exportReport(Object reportData, ReportFormatService<T> reportFormatService) {
-    return reportFormatService.export((T) reportData);
+  private <T> Object exportReport(String fileName, Object reportData,
+      ReportFormatService<T> reportFormatService) {
+
+    return reportFormatService.export(fileName, (T) reportData);
   }
 
   private Object search(ReportRequestDTO requestDTO, ReportTypeService<?> reportTypeService) {
     return reportTypeService.search(requestDTO);
+  }
+
+  private String generateFileName(ReportRequestDTO requestDTO) {
+    return fileUtil.generateReportingFileName(requestDTO);
   }
 }
