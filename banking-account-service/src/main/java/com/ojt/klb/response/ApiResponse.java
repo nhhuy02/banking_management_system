01aa6c59b101559fa17e51865b8cec3a237a5aad
Nguyễn Quốc Duy <127750881+Duy220807@users.noreply.github.com>
2024-10-01 10:22:56 +0700

    Revert "Merge branch 'NHH' into 'main'"
    This reverts commit 6f1f73ee33d8f92fbc0086e6128293ee44f7c82c, reversing
    changes made to 92fc51347cb388a9848b4f936cd9732be2ceb403.

--- banking-account-service/src/main/java/com/ojt/klb/response/ApiResponse.java
+++ banking-account-service/src/main/java/com/ojt/klb/response/ApiResponse.java
@@ -1,12 +1,10 @@
 package com.ojt.klb.response;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
-@Builder
 public class ApiResponse<T> {
     private int status;
     private String message;
