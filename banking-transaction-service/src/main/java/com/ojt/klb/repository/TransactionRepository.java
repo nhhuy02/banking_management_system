671f92466503852ccd7693ac8adac9d07a6fc096
huynh <huynh1@kielongbank.com>
2024-10-01 08:37:09 +0700

    Add transaction service

--- banking-transaction-service/src/main/java/com/ojt/klb/repository/TransactionRepository.java
+++ banking-transaction-service/src/main/java/com/ojt/klb/repository/TransactionRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-    List<Transaction> findByAccountNumber(String accountNumber);
+    List<Transaction> findByAccountNumber(Long accountNumber);
     List<Transaction> findByReferenceNumber(String referenceNumber);
 }
